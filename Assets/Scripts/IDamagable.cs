e6317cb4ee51afa5a60b9de7db3aba756db71f9f
Gerald Lang <some_1337@Gerald-Lang.local>
2016-08-06 18:58:32 -0400

    Damage interface updated

--- Assets/Scripts/IDamagable.cs
+++ Assets/Scripts/IDamagable.cs
@@ -2,8 +2,10 @@
 using System.Collections;
 
 public interface IDamagable {
-	bool IsVulnerable();
-
+	/// <summary>
+	/// Damage the specified amount. Return true if damage went through, false otherwise.
+	/// </summary>
+	/// <param name="amount">Amount.</param>
 	bool Damage(float amount);
 
 	float HealthRemaining();
